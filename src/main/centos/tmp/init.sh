8ef83abe348f15658e7aa4de0d8a3c8cb1739a78
jiangyuanlin <jiangyuanlin@163.com>
2019-12-12 09:47:25 +0800

    commit

---
+++ src/main/centos/tmp/init.sh
@@ -0,0 +1,13 @@
+!/bin/bash
+
+/usr/local/pgsql/bin/initdb -E utf8 -D /usr/local/pgsql/data 
+
+/usr/local/pgsql/bin/postgres    -D /usr/local/pgsql/data 
+ 
+# 添加root用户
+psql -p5432 -Upostgres -S -c "create user root with password '123456'" postgres
+
+# 修改postgres的密码
+psql -p5432 -Upostgres -S -c "ALTER USER postgres WITH PASSWORD '123456'" postgres
+
+ 

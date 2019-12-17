a3c8ae9f71439bd9e94c7244d8988a302056e35b
jiangyuanlin <jiangyuanlin@163.com>
2019-12-17 15:16:55 +0800

    commit

--- src/main/centos/tmp/init.sh
+++ src/main/centos/tmp/init.sh
@@ -2,6 +2,9 @@
 set -xev
 source /etc/profile
 
+# 设置环境变量 
+echo "export PATH=/usr/local/pgsql/bin:$PATH" >> /etc/profile && source /etc/profile
+
 #初始化
 mkdir -pv /usr/local/pgsql/data
 /usr/local/pgsql/bin/initdb -E utf8 -D /usr/local/pgsql/data
@@ -17,10 +20,10 @@ nohup /usr/local/pgsql/bin/postgres    -D /usr/local/pgsql/data 2>&1 </dev/null 
 #/usr/local/pgsql/bin/pg_ctl    -D /usr/local/pgsql/data start
 
 # 添加root用户
-psql -p5432 -Upostgres -S -c "create user root with password '123456'" postgres
+/usr/local/pgsql/bin/psql -p5432 -Upostgres -S -c "create user root with password '123456'" postgres
 
 # 修改postgres的密码
-psql -p5432 -Upostgres -S -c "ALTER USER postgres WITH PASSWORD '123456'" postgres
+/usr/local/pgsql/bin/psql -p5432 -Upostgres -S -c "ALTER USER postgres WITH PASSWORD '123456'" postgres
 
 # 登录
 #psql -h 127.0.0.1 -p 5432 -U root -d postgres
--- src/main/centos/tmp/install2.sh
+++ src/main/centos/tmp/install2.sh
@@ -1,13 +1,10 @@
 !/bin/sh
 
-# 下载postgresql
+# 下载
 cd /tmp && curl -O http://get.enterprisedb.com/postgresql/postgresql-9.4.9-1-linux-x64-binaries.tar.gz
 
 # 解压
 tar xzf postgresql-9.4.9-1-linux-x64-binaries.tar.gz -C /usr/local/
-
-# 设置环境变量 
-echo "export PATH=/usr/local/pgsql/bin:$PATH" >> /etc/profile && source /etc/profile
 
 # 创建用户
 groupadd postgres 

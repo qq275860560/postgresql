7f66d10dbebf42551051e23ff2a399b70e576f17
jiangyuanlin <jiangyuanlin@163.com>
2019-12-17 12:30:22 +0800

    commit

--- src/main/centos/tmp/init.sh
+++ src/main/centos/tmp/init.sh
@@ -2,9 +2,15 @@
 
 /usr/local/pgsql/bin/initdb -E utf8 -D /usr/local/pgsql/data 
 
-/usr/local/pgsql/bin/postgres    -D /usr/local/pgsql/data 
+# 修改监听地址和端口
+cd /usr/local/pgsql/data/ && curl -O https://raw.githubusercontent.com/qq275860560/postgresql/master/src/main/centos/usr/local/pgsql/data/postgresql.conf
 
-netstat -anltp
+
+# 修改远程访问权限
+cd /usr/local/pgsql/data/ && curl -O https://raw.githubusercontent.com/qq275860560/postgresql/master/src/main/centos/usr/local/pgsql/data/pg_hba.conf
+
+# 启动
+nohup  /usr/local/pgsql/bin/postgres    -D /usr/local/pgsql/data 2>&1 </dev/null &
  
 # 添加root用户
 psql -p5432 -Upostgres -S -c "create user root with password '123456'" postgres
